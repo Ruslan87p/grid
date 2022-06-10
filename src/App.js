32294e6cce968efb134e3e25a73ff7eb08f3f0a5
Ruslan Pl <plotkin1987@gmail.com>
2022-06-10 15:07:56 +0300

    grid + modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+
+
+function Grid() {
+
+  const [modal, setModal] = useState(false)
+  const closeModal = () => {
+    setModal(false)
+  }
+
+  const showModal = () => {
+    setModal(true)
+  }
+
+  useEffect(() => {
+      modal ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'unset'
+ }, [modal ]);
+
+  return(
+    <div className="wrapper">
+    <section className="main-section">
+        <div className="header">header</div>
+        <div className="languages-section">
+            languages-section
+            <br/>
+            languages-section
+            <br/>
+            languages-section
+            <br/>
+            languages-section
+            <br/>
+            languages-section
+            <br/>
+            languages-section
+            <br/>
+            languages-section
+            <br/>
+            languages-section
+            <br/>
+            languages-section
+            <br/>
+            languages-section
+            <br/>
+            languages-section
+            <br/>
+            languages-section
+            <br/>
+            languages-section
+            </div>
+          <div className="dependencies-section">modal-section <br /> 
+            <button onClick={() => showModal()}>OPEN MODAL</button>
+
+            {
+              modal ?  <Modal modal={modal} close={closeModal}></Modal>: null 
+            }
+
+          </div>
+        <div className="modal-section">dependencies-section</div>
+        <div className="footer">footer</div>
+    </section>
+  </div>
+  )
+}
+
+function Modal({close}) {
+  return(
+    <div className="overlay">
+      <div className="modal">
+        <button onClick={() => close()}>CLOSE MODAL</button>
+      </div>
+    </div>
+  )
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Grid></Grid>
     </div>
   );
 }
+
+
 
 export default App;
